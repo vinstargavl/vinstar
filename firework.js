564a085cd14e301a1b6121d0343d8017d090cdcc
vinstargavl <vinstarhiu@gmail.com>
2025-01-01 12:16:21 +0700

    firework.js

--- firework.js
+++ firework.js
@@ -4,7 +4,7 @@
 
 class Firework {
   constructor() {
-    this.hu = random(255);
+    this.hu = random(1000);
     this.firework = new Particle(random(width), height, this.hu, true);
     this.exploded = false;
     this.particles = [];
