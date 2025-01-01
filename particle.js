3953f1697acf28ea2e566983640ce24ab9297b78
vinstargavl <vinstarhiu@gmail.com>
2025-01-01 13:46:18 +0700

    particle.js

--- particle.js
+++ particle.js
@@ -5,7 +5,7 @@ class Particle {
     this.lifespan = 255;
     this.hu = hu;
     this.acc = createVector(0, 0);
-    this.size = firework ? 2 : random(2, 6); // Khởi tạo kích thước nhỏ hơn nếu là pháo hoa chính
+    this.size = firework ? 8 : random(2, 6); // Kích thước lớn hơn nếu là pháo hoa chính
     if (this.firework) {
       this.vel = createVector(0, random(-12, -8));
     } else {
@@ -19,10 +19,7 @@ class Particle {
   }
 
   update() {
-    if (this.firework) {
-      // Tăng kích thước khi pháo hoa đang bắn lên
-      this.size = lerp(this.size, 8, 0.1); // Lớn dần đến giá trị 8
-    } else {
+    if (!this.firework) {
       this.vel.mult(0.9);
       this.lifespan -= 4;
     }
@@ -46,7 +43,7 @@ class Particle {
       strokeWeight(this.size);
       stroke(this.hu, 255, 255, this.lifespan);
     } else {
-      strokeWeight(this.size);
+      strokeWeight(this.size); // Kích thước lớn cho pháo hoa chính
       stroke(this.hu, 255, 255);
     }
 
