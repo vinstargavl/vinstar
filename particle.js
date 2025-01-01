55e296d414c933a47b7e4c2a933c09b58de6c9a9
vinstargavl <vinstarhiu@gmail.com>
2025-01-01 14:46:24 +0700

    Update particle.js

--- particle.js
+++ particle.js
@@ -24,7 +24,7 @@ class Particle {
       this.vel.mult(0.9);  // Giảm tốc độ của các hạt nổ theo thời gian
       this.lifespan -= 4;  // Giảm độ trong suốt của các hạt
       // Giảm kích thước hạt theo thời gian
-      this.size = lerp(this.size, 0, 0.05);  // Dần dần giảm kích thước hạt nổ
+      this.size = lerp(this.size, 5, 0.05);  // Dần dần giảm kích thước hạt nổ
     }
     this.vel.add(this.acc);
     this.pos.add(this.vel);
