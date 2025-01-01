de0812f4c90a83cd6dbe8566f8fe0c6868d4e40e
vinstargavl <vinstarhiu@gmail.com>
2025-01-01 14:50:21 +0700

    Update particle.js

--- particle.js
+++ particle.js
@@ -5,7 +5,7 @@ class Particle {
     this.lifespan = 255;
     this.hu = hu;
     this.acc = createVector(0, 0);
-    this.size = firework ? 20 : random(2, 6); // Kích thước ban đầu của hạt nổ
+    this.size = firework ? 20 : random(5, 12);  // Kích thước ban đầu của hạt nổ
 
     if (this.firework) {
       this.vel = createVector(0, random(-12, -8)); // Pháo hoa bắn lên
@@ -21,10 +21,11 @@ class Particle {
 
   update() {
     if (!this.firework) {
+      // Làm cho kích thước hạt nổ dần lớn lên theo thời gian
+      this.size = lerp(this.size, random(5, 10), 0.05);  // Tăng kích thước theo thời gian
+
       this.vel.mult(0.9);  // Giảm tốc độ của các hạt nổ theo thời gian
       this.lifespan -= 4;  // Giảm độ trong suốt của các hạt
-      // Giảm kích thước hạt theo thời gian
-      this.size = lerp(this.size, 5, 0.05);  // Dần dần giảm kích thước hạt nổ
     }
     this.vel.add(this.acc);
     this.pos.add(this.vel);
